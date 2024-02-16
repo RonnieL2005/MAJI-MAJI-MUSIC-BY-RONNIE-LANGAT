3d1acf07f441f2fa3e5e5142a6f9259e5e2ab52b
Ronnielangat <ronnielangat2020@gmail.com>
2024-02-16 04:21:21 +0300

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Search from "./Search";
+import DarkmodeLightmode from "./DarkmodeToggle";
+import "./App.css"; 
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`App ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+      <div className="purple-dashboard"> 
+        <h1 className="text-white text-center py-3">Maji Maji Music</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="full-height"> 
+        <Search />
+        <DarkmodeLightmode isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/DarkmodeToggle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function DarkmodeLightmode({ isDarkMode, toggleDarkMode }) {
+  return (
+    <Button onClick={toggleDarkMode} variant={isDarkMode ? "light" : "dark"}>
+      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    </Button>
+  );
+}
+
+export default DarkmodeLightmode;
